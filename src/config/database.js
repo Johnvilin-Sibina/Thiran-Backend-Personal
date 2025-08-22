bc555eb96bc9344c4a42d19e8ca3f1b0f08d1094
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2025-08-22 22:58:06 +0530

    Createddatabase.js file

---
+++ src/config/database.js
@@ -0,0 +1,5 @@
+import { Sequelize } from "sequelize";
+import config from "./config.cjs";
+const env = process.env.NODE_ENV || "development";
+const sequelize = new Sequelize(config[env]);
+export default sequelize;
